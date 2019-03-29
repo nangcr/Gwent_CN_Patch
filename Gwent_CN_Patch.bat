0371dccae28d3f4a66af990f642628fc09fa1b2d
Nangcr <nangcr@gmail.com>
2019-03-29 23:32:26 +0800

    Game version 2.0.0

---
+++ Gwent_CN_Patch.bat
@@ -0,0 +1,53 @@
+:::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Gwent_CN_patch.bat - For Game Version 2.0.0
+:: Replace services and localization configuration
+:::::::::::::::::::::::::::::::::::::::::::::::::::
+ @echo off
+ CLS
+
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~0"
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  NET FILE 1>NUL 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+  ECHO.
+  ECHO **************************************
+  ECHO Invoking UAC for Privilege Escalation
+  ECHO **************************************
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+ set /p op=OUTPUT_PATH:
+ copy "%~dp0\Gwent_Data\StreamingAssets\services\services.xml" "%op%\Gwent_Data\StreamingAssets\services\services.xml"
+ xcopy "%~dp0\Gwent_Data\StreamingAssets\localization" "%op%\Gwent_Data\StreamingAssets\localization" /s /e /y
---
+++ readme.md
@@ -0,0 +1,35 @@
+Gwent_CN_Patch
+==============
+
+为《巫师之昆特牌》国际版客户端提供汉化的脚本
+
+## 下载
+
+请使用git命令clone本仓库或直接点击[此处](https://github.com/Nangcr/gwent_cn_patch/archive/master.zip)开始下载
+
+## 使用
+
+使用此补丁时，需要手动设置输出目录，你需要复制国际版昆特牌客户端的安装目录
+启动GOG Galaxy客户端并选中GWENT:The Witcher Card Game页面
+依次点击 更多-管理安装文件-显示文件夹
+手动复制资源管理器中显示的目录地址，它可能像下面这样：
+
+    D:\GOG Galaxy\Games\Gwent
+
+然后在脚本运行后进行粘贴（推荐单击鼠标右键）随后敲击回车即可
+执行完成后，请直接启动国际版客户端，并在设置（右下角齿轮）中将Text Language更替为简体中文
+
+## 注意事项
+
+出于便于国服玩家使用的考虑，本脚本将会同时覆盖服务器配置文件和汉化文件
+如果您想在国际服进行游戏而非国服，请手动备份国际版客户端中的services.xml文件
+
+## License
+
+Gwent_CN_Patch is licensed under the [MIT License](LICENSE). You are encouraged
+to embed Gwent_CN_Patch into your other projects, as long as the license
+permits.
+
+You are also encouraged to disclose your improvements to the public, so that
+others may benefit from your modification, in the same way you receive benefits
+from this project.
